9eb11bdfd8b4310d7dfa110da2c93c1136afb685
teru12012000 <sion2769@gmail.com>
2024-12-17 06:19:43 +0900

    create project

---
+++ hello-world/target/debug/deps/hello_world-b1dfab825f7d9800.d
@@ -0,0 +1,5 @@
+/Users/terashieishion/Desktop/tutorial/rust-tutorial/hello-world/target/debug/deps/hello_world-b1dfab825f7d9800: src/main.rs
+
+/Users/terashieishion/Desktop/tutorial/rust-tutorial/hello-world/target/debug/deps/hello_world-b1dfab825f7d9800.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/terashieishion/Desktop/tutorial/rust-tutorial/hello-world/target/debug/hello-world: /Users/terashieishion/Desktop/tutorial/rust-tutorial/hello-world/src/main.rs
