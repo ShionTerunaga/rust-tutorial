f0f74497fa16f258d9d95955a34661b5cec86073
teru12012000 <sion2769@gmail.com>
2024-12-21 13:10:03 +0900

    feat: add function practice

---
+++ about-function/src/main.rs
@@ -0,0 +1,16 @@
+use rand::Rng;
+
+fn judge_return_number(num:u32){
+    let secret_number=rand::thread_rng().gen_range(1..100);
+
+    if num < secret_number{
+        println!("秘密の数字は50より小さいです");
+    } else {
+        println!("秘密の数字は50より大きいです");
+    }
+}
+
+fn main() {
+    judge_return_number(50);
+    
+}
