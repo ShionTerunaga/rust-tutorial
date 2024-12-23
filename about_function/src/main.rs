fc3b1ef2354c30eefef8f1f125eaefd515404d42
teru12012000 <sion2769@gmail.com>
2024-12-23 22:03:58 +0900

    fix: formatted

--- about_function/src/main.rs
+++ about_function/src/main.rs
@@ -1,9 +1,9 @@
 use rand::Rng;
 
-fn judge_return_number(num:u32){
-    let secret_number=rand::thread_rng().gen_range(1..100);
+fn judge_return_number(num: u32) {
+    let secret_number = rand::thread_rng().gen_range(1..100);
 
-    if num < secret_number{
+    if num < secret_number {
         println!("秘密の数字は50より小さいです");
     } else {
         println!("秘密の数字は50より大きいです");
@@ -12,5 +12,4 @@ fn judge_return_number(num:u32){
 
 fn main() {
     judge_return_number(50);
-    
 }
