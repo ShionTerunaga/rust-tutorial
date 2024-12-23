d30185c4c026cfbd23333b1baeee3dc98cf0c81c
teru12012000 <sion2769@gmail.com>
2024-12-23 22:16:17 +0900

    feat: add control program

--- control_program/src/main.rs
+++ control_program/src/main.rs
@@ -1,3 +1,8 @@
+use rand::Rng;
 fn main() {
-    println!("Hello, world!");
+    let rand=rand::thread_rng().gen_range(1..100);
+    //本当はそういうことはしないけど
+    let judge= if rand%2==0 {"偶数だ！"} else {"奇数だ！"};
+
+    println!("{}!!これは{}",rand,judge);
 }
