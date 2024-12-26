b9e904aed01256ed98f3350495a15ae1de00d32f
teru12012000 <sion2769@gmail.com>
2024-12-26 20:19:19 +0900

    feat: base

---
+++ ownership/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    // part1
+    /*
+    ここにまとめ
+     */
+    //let mut s =String::from("hello");
+    //
+    //s.push_str("world");
+    //
+    //println!("{}",s);
+
+    // part2
+    /*
+    まとめ
+     */
+    //let s1 = String::from("hello");
+    //
+    //let s2 = s1;
+    //
+    //println!("{}", s2)
+
+    // part3
+    /*
+    ここにまとめ
+     */
+    //let s1=String::from("hello");
+    //
+    //let s2=s1.clone();
+    //
+    //println!("s1={}, s2={}",s1,s2);
+
+    //part4
+    /*
+    ここにまとめ
+     */
+
+    let x = 5;
+    let y = x;
+
+    println!("x = {} , y = {}", x, y);
+}
+
+//一旦ここまで
