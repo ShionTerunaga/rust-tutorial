e563be13791a906afeaf9b8b54aa3f2b64caaf25
teru12012000 <sion2769@gmail.com>
2024-12-29 03:07:52 +0900

    fix: delete comment

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,38 +1,22 @@
 fn main() {
-    // part1
-    /*
-    ここにまとめ
-     */
-    //let mut s =String::from("hello");
-    //
-    //s.push_str("world");
-    //
-    //println!("{}",s);
+    let mut s =String::from("hello");
+    
+    s.push_str("world");
+    
+    println!("{}",s);
 
-    // part2
-    /*
-    まとめ
-     */
-    //let s1 = String::from("hello");
-    //
-    //let s2 = s1;
-    //
-    //println!("{}", s2)
+    
+    let s1 = String::from("hello");
+    
+    let s2 = s1;
+    
+    println!("{}", s2);
 
-    // part3
-    /*
-    ここにまとめ
-     */
-    //let s1=String::from("hello");
-    //
-    //let s2=s1.clone();
-    //
-    //println!("s1={}, s2={}",s1,s2);
-
-    //part4
-    /*
-    ここにまとめ
-     */
+    let s1=String::from("hello");
+    
+    let s2=s1.clone();
+    
+    println!("s1={}, s2={}",s1,s2);
 
     let x = 5;
     let y = x;
